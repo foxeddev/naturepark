c611b38d9fdf63503b269939b74f0eaaaeb8ba63
Rotsteinfuchs <169158948+Rotsteinfuchs@users.noreply.github.com>
2025-03-04 11:06:00 +0100

    Add configurable project namespaces and refactor function tag creation

--- plugins/function_tags.py
+++ plugins/function_tags.py
@@ -1,17 +1,38 @@
-from beet import Context, FunctionTag
+import os
+from pathlib import PosixPath
+from typing import Optional
+
+from beet import Context, FunctionTag, PluginOptions, configurable
+
+
+class FunctionTagOptions(PluginOptions):
+    project_namespaces: Optional[list[str]] = None
 
 
 def beet_default(ctx: Context):
-    namespaces = ctx.meta.get("project_namespaces")
+    ctx.require(function_tags)
 
-    if not namespaces:
-        raise ValueError("'project_namespaces' key missing in Beet meta.")
+
+@configurable(validator=FunctionTagOptions)
+def function_tags(ctx: Context, opts: FunctionTagOptions):
+    namespaces = opts.project_namespaces or find_namespaces(
+        os.path.join(ctx.directory, "src/data")
+    )
 
     for namespace in namespaces:
-        ctx.data.function_tags[f"{namespace}:load"] = FunctionTag(
-            {"values": [f"{namespace}:load"]}
-        )
+        ctx.data.function_tags[f"{namespace}:load"] = create_load_tag(namespace)
+        ctx.data.function_tags[f"{namespace}:tick"] = create_tick_tag(namespace)
 
-        ctx.data.function_tags[f"{namespace}:tick"] = FunctionTag(
-            {"values": [f"{namespace}:tick"]}
-        )
+
+def find_namespaces(directory: PosixPath):
+    for item in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, item)):
+            yield item
+
+
+def create_load_tag(namespace: str):
+    return FunctionTag({"values": [f"{namespace}:load"]})
+
+
+def create_tick_tag(namespace: str):
+    return FunctionTag({"values": [f"{namespace}:tick"]})
